c0b46b24862dd97138de8adb24398196286a4311
DAISUKE <42408108+IUCHIDAISUKE@users.noreply.github.com>
2021-09-02 11:31:12 +0900

    Arrow keys

--- main.c
+++ main.c
@@ -14,6 +14,14 @@
 #define ROKUVIM_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k)&0x1f)
 
+enum editorKey
+{
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+};
+
 /*** data ***/
 
 struct editorConfig
@@ -60,7 +68,7 @@ void enableRawMode()
         die("tcsetattr");
 }
 
-char editorReadKey()
+int editorReadKey()
 {
     int nread;
     char c;
@@ -69,7 +77,36 @@ char editorReadKey()
         if (nread == -1 && errno != EAGAIN)
             die("read");
     }
-    return c;
+
+    if (c == '\x1b')
+    {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1)
+            return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1)
+            return '\x1b';
+
+        if (seq[0] == '[')
+        {
+            switch (seq[1])
+            {
+            case 'A':
+                return ARROW_UP;
+            case 'B':
+                return ARROW_DOWN;
+            case 'C':
+                return ARROW_RIGHT;
+            case 'D':
+                return ARROW_LEFT;
+            }
+        }
+        return '\x1b';
+    }
+    else
+    {
+        return c;
+    }
 }
 
 int getCursolPosition(int *rows, int *cols)
@@ -148,20 +185,20 @@ void abFree(struct abuf *ab)
 
 /*** input ***/
 
-void editorMoveCursol(char key)
+void editorMoveCursol(int key)
 {
     switch (key)
     {
-    case 'a':
+    case ARROW_LEFT:
         E.cx--;
         break;
-    case 'd':
+    case ARROW_RIGHT:
         E.cx++;
         break;
-    case 'w':
+    case ARROW_UP:
         E.cy--;
         break;
-    case 's':
+    case ARROW_DOWN:
         E.cy++;
         break;
     }
@@ -169,7 +206,7 @@ void editorMoveCursol(char key)
 
 void editorProcessKeypress()
 {
-    char c = editorReadKey();
+    int c = editorReadKey();
 
     switch (c)
     {
@@ -179,10 +216,10 @@ void editorProcessKeypress()
         exit(0);
         break;
 
-    case 'w':
-    case 's':
-    case 'a':
-    case 'd':
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
         editorMoveCursol(c);
         break;
     }
