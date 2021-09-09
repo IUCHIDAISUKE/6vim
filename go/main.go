b0270caf158b3c32072ade3b69649c5cd2ccfdd1
DAISUKE <42408108+IUCHIDAISUKE@users.noreply.github.com>
2021-09-09 13:12:15 +0900

    [Golang] Turn off echoing

--- go/main.go
+++ go/main.go
@@ -3,9 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
+func enableRawMode() {
+	termios, err := unix.IoctlGetTermios(syscall.Stdin, unix.TIOCGETA)
+	if err != nil {
+		panic(err)
+	}
+
+	termios.Lflag &^= unix.ECHO
+
+	//TODO TIOCSETA is maybe system dependent code? You can use TCSETA?
+	if err := unix.IoctlSetTermios(syscall.Stdin, unix.TIOCSETA, termios); err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+	enableRawMode()
+
 	c := make([]byte, 1)
 	for {
 		os.Stdin.Read(c)
